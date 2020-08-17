3fdb7be98e88acef663a10cdaced97732065acdb
dalinwanglhtz <dalinwanglhtz@gmail.com>
2020-08-17 23:06:17 +1000

    anagrams submitted

--- anagrams.js
+++ anagrams.js
@@ -1,6 +1,14 @@
 // https://www.codewars.com/kata/523a86aa4230ebb5420001e1/javascript
 
 function anagrams(word, words) {
-    const rea = word.split('').sort((a,b)=> a < b ? -1 : 1).join('');
-    return words.filter(a => rea === a.split('').sort((a,b) => a< b ? -1 : 1).join(''));
+    const rea = reorder(word);
+    return words.filter(a => rea === reorder(a));
+}
+
+function reorder(word) {
+    // My solution
+    return word.split('').sort((a,b) => a < b ? -1 : 1).join('');
+
+    // Other solution
+    //return word.split('').sort().join('');
 }
