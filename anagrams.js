7c003514058f64dc3614afaf5f01c62259ba739f
dalinwanglhtz <dalinwanglhtz@gmail.com>
2020-08-17 22:50:08 +1000

    anagrams refactored

--- anagrams.js
+++ anagrams.js
@@ -1,11 +1,6 @@
 // https://www.codewars.com/kata/523a86aa4230ebb5420001e1/javascript
 
 function anagrams(word, words) {
-    let results = [];
     const rea = word.split('').sort((a,b)=> a < b ? -1 : 1).join('');
-    for(let a of words) {
-        if(rea === a.split('').sort((a,b) => a< b ? -1 : 1).join(''))
-            results.push(a);
-    }
-    console.log(results);
+    return words.filter(a => rea === a.split('').sort((a,b) => a< b ? -1 : 1).join(''));
 }
